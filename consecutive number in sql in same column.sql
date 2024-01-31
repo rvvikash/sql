69759c96753cdee4709c712b8e34e61fb32295a8
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-01-31 18:44:48 +0530

    Create consecutive number in sql in same column.sql

---
+++ consecutive number in sql in same column.sql
@@ -0,0 +1,50 @@
+Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+
+
+with cte as (
+select id,num,row_number() over(order by id)-row_number() over (partition by num order by id) as rn
+from logs)
+select distinct num as ConsecutiveNums from cte group by rn,num
+having count(*)>=3
