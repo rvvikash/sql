15a4dac8e25d0e173af76ebdc3b80a0973536d97
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-04-24 21:18:43 +0530

    Update intervie questions.sql

--- intervie questions.sql
+++ intervie questions.sql
@@ -1,4 +1,4 @@
-copy the data format without copying data
+*** copy the data format without copying data
 
 
 new_table is the name of the new table you want to create.
@@ -8,3 +8,8 @@ SELECT * selects all columns from the original table.
 WHERE 1 = 0 
 
 ensures that no rows are selected (since 1 is never equal to 0), but the structure of the selected columns remains intact.
+
+
+  *** unique constraint vs primary key
+
+
