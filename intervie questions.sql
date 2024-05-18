237578b202ad6b844a258a6debef9ad8b92bad5f
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-05-18 22:42:56 +0530

    Update intervie questions.sql

--- intervie questions.sql
+++ intervie questions.sql
@@ -14,19 +14,34 @@ ensures that no rows are selected (since 1 is never equal to 0), but the structu
 
 a unique constraint allows NULL but primary contain not null this is the huge diff 
 
-create index for table
-*** CREATE INDEX idx_last_name ON employees(last_name);
-
 
 -- merge statment in table 
-group by with two column 
-
-
- 
+group by with two column  
 
 SELECT emp_id, emp_name, SUM(salary) AS total_salary
 FROM employees
 GROUP BY emp_id, emp_name;
+
+moving sum()
+select a.*,sum(salary)over(order by emp_id asc) as tt from employees a 
+
+To calculate the moving sum from all previous values in SQL, 
+you can use a window function along with the ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW clause.
+This clause defines the window for the window function to include all rows from the beginning of the partition up to the current row. 
+ Here's an example query:
+
+ SELECT
+    date,
+    value,
+    SUM(value) OVER (ORDER BY date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS moving_sum_all_previous
+FROM
+    your_table
+ in order by put those column which increasing and behalf that we want to add so we used here date.
+
+ if we want to change the moving sum from last 4 days we can replace the unbounded with number. 
+ 6 preceding like that .
+
+
 
 
 
