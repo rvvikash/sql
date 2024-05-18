a89d0d251b5839c86f4a56ca713f8fd24f9d47e3
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-05-18 22:18:41 +0530

    Update and rename index to index.sql

--- index.sql
+++ index.sql
@@ -9,6 +9,19 @@ but in Non-Clustered index, there is separate index storage for the index.
 Clustered index offers faster data access, on the other hand, the Non-clustered index is slower.
 
 
+when we create a primary key then it automatically behave as index .
+
+if there is no index define in table it will scan full scan to retrive the data.
+
+
+B-tree indexes organize data in a balanced tree structure,
+allowing for efficient search, insertion, and deletion operations.
+
+CREATE INDEX index_name
+ON table_name (column_name);
+
+
+
 
 
 
