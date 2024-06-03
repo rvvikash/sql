9174e1af4a19adf6fc48bc9870fec2e9115e52ca
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-06-03 19:55:46 +0530

    Update date+time.sql

--- date+time.sql
+++ date+time.sql
@@ -7,3 +7,12 @@ AND EXTRACT(MONTH FROM SENT_DATE)='08'
 GROUP BY sender_id order by count(sender_id) desc
 limit 2
 https://datalemur.com/questions/teams-power-users
+
+
+in sql|
+
+DATEDIFF(day, required_date, shipped_date)
+
+SELECT 
+    DATEADD(second, 1, '2018-12-31 23:59:59') result;
+
