d491ad80272980ef4d8c54caf86d5e5d97c152ea
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-06-23 16:39:53 +0530

    Update date+time.sql

--- date+time.sql
+++ date+time.sql
@@ -1,6 +1,6 @@
 we used extract function to fetch the day month year time in postgresql.
 
-  SELECT sender_id, COUNT(SENDER_ID) 
+SELECT sender_id, COUNT(SENDER_ID) 
 FROM messages 
 WHERE EXTRACT(YEAR FROM sent_date)='2022'
 AND EXTRACT(MONTH FROM SENT_DATE)='08'
@@ -16,7 +16,16 @@ DATEDIFF(day, required_date, shipped_date)
 SELECT 
     DATEADD(second, 1, '2018-12-31 23:59:59') result;
 
+  DATEPART(year, shipped_date) [year], 
+  DATEPART(quarter, shipped_date) [quarter], 
+  DATEPART(month, shipped_date) [month], 
+  DATEPART(day, shipped_date) [day]
+  SELECT TIMEDIFF('2018-05-23 18:00:00', '2018-05-23 12:00:00')
+  As Result;
+
 
   THEN SEC_TO_TIME(TIMESTAMPDIFF(SECOND, Arrival, Next_Arrival))
+
+
 
 
