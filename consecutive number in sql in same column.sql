b60b2951b236ff5b9478d4648da28da4c00b86cc
rvvikash <38788306+rvvikash@users.noreply.github.com>
2026-01-21 22:14:37 +0530

    Update consecutive number in sql in same column.sql

--- consecutive number in sql in same column.sql
+++ consecutive number in sql in same column.sql
@@ -47,4 +47,9 @@ with cte as (
 select id,num,row_number() over(order by id)-row_number() over (partition by num order by id) as rn
 from logs)
 select distinct num as ConsecutiveNums from cte group by rn,num
-having count(*)>=3
+having count(*)>=3'
+
+
+ PARTITION BY groups all equal values, not consecutive ones
+
+ firstly it created partition by num so assign value after order it change the place .
